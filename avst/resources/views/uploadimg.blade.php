586cdf69e02c0a9ea8ffbcbcfc75ded4b39d1d97
Rasika <rasikat77@gmail.com>
2018-06-11 18:18:34 +0530

    Software uploading

--- avst/resources/views/uploadimg.blade.php
+++ avst/resources/views/uploadimg.blade.php
@@ -1,34 +1,34 @@
 @extends('layouts.web')
 
 @section('content')
-    <div class="col-md-8 col-md-offset-2">
-        <h1 class="panel-heading">Update Code to Units</h1>
-        <div class="panel panel-default">
 
-            <div></div>
-
-            <div class="panel-body">
-                <form class="form-horizontal" method="GET" action="/uoload/code">
+    <h1 class="panel-heading">Update Code to Units</h1>
+    <div class="panel panel-default">
+        <div></div>
+        <div class="row h-100">
+            <div class="col-sm-6 h-100">
+                <form class="form-horizontal" method="GET" action="/upload/code">
 
 
                     <div class="form-group">
                         <label for="email" class="col-md-4 control-label">Software Version</label>
                         <div class="col-md-6">
-                            <input id="author" type="text" class="form-control" name="Author" required autofocus>
+                            <input id="author" type="text" class="form-control" name="version" required autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="email" class="col-md-4 control-label">Comments</label>
                         <div class="col-md-6">
-                            <input id="author" type="text" class="form-control" name="Author" required autofocus>
+                            <input id="author" type="text" class="form-control" name="comment" required autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="email" class="col-md-4 control-label">Code(*.py)</label>
-                        <div class="col-md-6">
-                            <textarea id="author" rows="20" type="text" class="form-control" name="Author" required autofocus></textarea>
+                        <div class="">
+                            <textarea id="author" rows="20" type="text" class="form-control" name="code" required
+                                      autofocus></textarea>
                         </div>
                     </div>
 
@@ -41,6 +41,27 @@
                     </div>
                 </form>
             </div>
+            <div class=" col-sm-6 h-100">
+                <div class="row">
+                    @foreach ($items as $item)
+
+                        <div class="alert alert-dark " style="width: 90%" role="alert">
+                            <h4 class="alert-heading">{{$item->version}}</h4>
+                            <hr>
+                            <p>{{$item->comment}}</p>
+
+                            <p class="mb-0">{{$item->updated_at}}</p>
+                            <div class="float-right">
+                                <a href="/deleteNews/{{$item->id}}">X</a>
+                            </div>
+                        </div>
+                    @endforeach
+
+                </div>
+            </div>
         </div>
     </div>
+
+
+
 @endsection
