653df1b2e31821b4126349e6f14ecd903e145a2b
Rasika <rasikat77@gmail.com>
2018-06-11 18:27:20 +0530

    Software uploading added files

---
+++ avst/app/Http/Controllers/CodeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Code;
+use Illuminate\Http\Request;
+
+class CodeController extends Controller
+{
+    public function getCodes(){
+        $code=Code::all();
+        return view('uploadimg',['items'=>$code->reverse()]);
+    }
+
+    public function uploadCode(Request $request){
+        $code= new Code();
+        $code->code=$request->input("code");
+        $code->comment=$request->input("comment");
+        $code->version=$request->input("version");
+        $code->author="NOT DEFINED";
+        $code->save();
+        return $this->getCodes();
+        //return redirect("/addNew");
+    }
+
+
+    public function deleteCode( $id){
+        $code=(new \App\Code())->find($id);
+        $code->delete();
+        return redirect("/upload/code");
+
+    }
+}
---
+++ avst/database/migrations/2018_06_11_113553_create_codes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            //$table->string("Code");
+            $table->text("code");
+            $table->string("comment");
+            $table->string("version");
+            $table->string("author");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
